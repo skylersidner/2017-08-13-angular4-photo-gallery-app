d0ce376139f5ced11edd6abedc6d0386303c2139
Skyler Sidner <skyler.sidner@atssecured.com>
2017-08-13 12:06:03 -0500

    added import to app component module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
