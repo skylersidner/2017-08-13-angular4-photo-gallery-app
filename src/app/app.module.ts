8645eedb86e92e4cf5e80e0e48223fb57d646d92
Skyler Sidner <skyler.sidner@atssecured.com>
2017-08-13 12:16:23 -0500

    bootstrap styling and components working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AlertModule
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
